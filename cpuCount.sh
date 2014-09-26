a37903198f6fe65fb7be2a0cbf8433415b24ca39
x00093357 <x00093357@ittd.ie>
2014-09-26 15:43:52 +0100

    converted to executable file

--- cpuCount.sh
+++ cpuCount.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+#: Title : cpuCount.sh
+#: Date : 26/09/2014
+#: Author : x00093357@ittd.ie
+#: Version : 1.0
+#: Description : CPU count script 
+#: Options :
+
 grep processor /proc/cpuinfo | wc -l
